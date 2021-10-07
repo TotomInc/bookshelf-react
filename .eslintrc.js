3f0cc7b53539c36a20c3e624e7b8a67f9375d9a9
Thomas Cazade <cazade.thomas@gmail.com>
2021-10-07 19:40:50 +0200

    Update ESLint config

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,9 +1,3 @@
 module.exports = {
   extends: ['@totominc/react'],
-
-  // Add this part if you are using the ESLint config on a TypeScript project
-  parserOptions: {
-    tsconfigRootDir: __dirname,
-    project: ['./tsconfig.json'],
-  },
 };
